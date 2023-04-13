c9ca5671c2c73f76161366dd32710b11ad7afd19
mikedel <mcdelsi@gmail.com>
2023-04-13 09:18:39 -0400

    added everything including lazy lock

---
+++ after/plugin/defaults.lua
@@ -0,0 +1,22 @@
+-- line number
+vim.opt.relativenumber = true
+
+-- indent and wrap
+vim.opt.autoindent = true
+vim.opt.wrap = false
+
+-- highlight current line
+vim.opt.cursorline = true
+
+-- backspace
+vim.opt.backspace = "indent,eol,start"
+
+-- clipboard
+
+
+-- split windows
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- make '-' not a word delimeter
+--vim.opt.iskeyword:append("-")
