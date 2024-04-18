ffc7b2a5162003529fcf948dbc2f2aba9d1fe753
mikedel <mcdelsi@gmail.com>
2024-04-18 11:34:54 -0400

    quick fix plugin

--- init.lua
+++ init.lua
@@ -217,6 +217,9 @@ require('lazy').setup({
     }
   },
 
+  -- Quickfix Reflector to allow editable quickfix list
+  'stefandtw/quickfix-reflector.vim',
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
